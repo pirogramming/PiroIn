bf9f3fa00f462e22ed8dfda388fceee0ab9cda7f
lilyyang0077 <lilyyang0077@test.com>
2026-05-09 21:52:47 +0900

    fix: Role enums 디렉션 수정

--- backend/src/main/java/com/example/Piroin/project/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/example/Piroin/project/domain/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.example.Piroin.project.domain.user.repository;
 
-import com.example.Piroin.project.domain.user.entity.Role;
+import com.example.Piroin.project.domain.user.enums.Role;
 import com.example.Piroin.project.domain.user.entity.User;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;

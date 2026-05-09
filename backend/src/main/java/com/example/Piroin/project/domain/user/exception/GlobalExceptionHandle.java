18f68fc5f0c2753ab87fc214d667a067f7621aeb
lilyyang0077 <lilyyang0077@test.com>
2026-05-09 21:48:28 +0900

    docs: 피로체크 코드 가져오기 & 간단 수정

---
+++ backend/src/main/java/com/example/Piroin/project/domain/user/exception/GlobalExceptionHandle.java
@@ -0,0 +1,27 @@
+package com.example.Piroin.project.domain.user.exception;
+
+import com.example.Piroin.project.domain.attendance.dto.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // InvalidLoginException (로그인 실패)
+    @ExceptionHandler(InvalidLoginException.class)
+    public ResponseEntity<ApiResponse<?>> handleInvalidLoginException(InvalidLoginException e) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED) // 401 상태 코드
+                .body(ApiResponse.error(e.getMessage())); // 에러 메시지 전달
+    }
+
+    // RuntimeException (유저관리 상세페이지)
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiResponse<?>> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST) // 404
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+}
--- backend/src/main/java/com/example/Piroin/project/domain/user/service/UserService.java
+++ backend/src/main/java/com/example/Piroin/project/domain/user/service/UserService.java
@@ -1,4 +1,26 @@
 package com.example.Piroin.project.domain.user.service;
 
+import com.example.Piroin.project.domain.user.entity.User;
+import com.example.Piroin.project.domain.user.exception.InvalidLoginException;
+import com.example.Piroin.project.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User login(String name, String password) {
+        User user = userRepository.findByName(name)
+                .orElseThrow(() -> new InvalidLoginException("해당 사용자가 존재하지 않습니다.")); //401
+
+        if (!user.getPassword().equals(password)) {
+            throw new InvalidLoginException("비밀번호가 일치하지 않습니다."); //401
+        }
+
+        return user;
+    }
 }
+

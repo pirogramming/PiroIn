6dab1918b4b9152adb5ace99c9bd4d1f979b1e3f
plumbestie <jangmj1121@naver.com>
2026-05-10 11:29:19 +0900

    Feat : 로그인 및 온보딩 페이지 UI  뼈대 설계

---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+function LoginPage() {
+  return (
+    <div>
+      <h1>로그인</h1>
+      <input type="text" placeholder="아이디" />
+      <input type="password" placeholder="비밀번호" />
+      <button>로그인</button>
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ frontend/src/pages/OnboardingPage.js
@@ -0,0 +1,9 @@
+function OnboardingPage() {
+  return (
+    <div>
+      <h1>온보딩</h1>
+    </div>
+  );
+}
+
+export default OnboardingPage;

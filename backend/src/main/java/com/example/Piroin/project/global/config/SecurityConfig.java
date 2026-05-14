5ed1838fa0a0ae6b8ab7b6b1f3a9246c641dc371
xihxxn <sihyun8870@naver.com>
2026-05-15 01:38:23 +0900

    [Fix] 모든 엔드포인트 임시 허용 설정

--- backend/src/main/java/com/example/Piroin/project/global/config/SecurityConfig.java
+++ backend/src/main/java/com/example/Piroin/project/global/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/login", "/api/curriculums").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtUtil),
                         UsernamePasswordAuthenticationFilter.class)

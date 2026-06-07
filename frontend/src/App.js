b2cb122404bb5aff05b1da3bc7b5ae19a44fe547
lilyyang0077 <lilyyang0077@test.com>
2026-06-07 10:44:07 +0900

    feat: 로그인 리다이랙트 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import Assignment from './pages/pirocheck/assignment/Assignment';
 import Deposit from './pages/pirocheck/deposit/Deposit';
 import StudentList from './pages/pirocheck/students/StudentList';
 import StudentDetail from './pages/pirocheck/students/StudentDetail';
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
   return (
@@ -23,22 +24,27 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/onboarding" element={<OnboardingPage />} />
 
-        {/* 라이트 헤더 페이지 */}
-        <Route element={<Layout headerType="light" />}>
-          <Route path="/sessions" element={<QnAMainPage />} />
-          <Route path="/sessions/:sessionId/questions" element={<QnAListPage />} />
-          <Route path="/sessions/:sessionId/questions/:questionId" element={<QnADetailPage />} />
-          <Route path="/curriculum" element={<CurriculumPage />} />
-        </Route>
+        {/* 로그인 필요한 페이지들 */}
+        <Route element={<ProtectedRoute />}>
 
-        {/* 다크 헤더 페이지 */}
-        <Route element={<Layout headerType="dark" />}>
-          <Route path="/pirocheck" element={<PiroCheckMain />} />
-          <Route path="/pirocheck/attendance" element={<Attendance />} />
-          <Route path="/pirocheck/assignment" element={<Assignment />} />
-          <Route path="/pirocheck/deposit" element={<Deposit />} />
-          <Route path="/pirocheck/students" element={<StudentList />} />
-          <Route path="/pirocheck/students/:userId" element={<StudentDetail />} />
+          {/* 라이트 헤더 페이지 */}
+          <Route element={<Layout headerType="light" />}>
+            <Route path="/sessions" element={<QnAMainPage />} />
+            <Route path="/sessions/:sessionId/questions" element={<QnAListPage />} />
+            <Route path="/sessions/:sessionId/questions/:questionId" element={<QnADetailPage />} />
+            <Route path="/curriculum" element={<CurriculumPage />} />
+          </Route>
+
+          {/* 다크 헤더 페이지 */}
+          <Route element={<Layout headerType="dark" />}>
+            <Route path="/pirocheck" element={<PiroCheckMain />} />
+            <Route path="/pirocheck/attendance" element={<Attendance />} />
+            <Route path="/pirocheck/assignment" element={<Assignment />} />
+            <Route path="/pirocheck/deposit" element={<Deposit />} />
+            <Route path="/pirocheck/students" element={<StudentList />} />
+            <Route path="/pirocheck/students/:userId" element={<StudentDetail />} />
+          </Route>
+
         </Route>
 
       </Routes>
